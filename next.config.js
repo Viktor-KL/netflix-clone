bc150429ee0f040726cd9202b237fb47756c745f
Viktor-KL <viktorkyrylovych88@gmail.com>
2023-01-26 11:44:21 +0200

    Solving the problem with API for banner

--- next.config.js
+++ next.config.js
@@ -2,13 +2,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-        port: '',
-        pathname: 'https://image.tmdb.org/t/p/original/',
-      },
+    loader: "default",
+    minimumCacheTTL: 60,
+    domains: [
+      "image.tmdb.org"
     ],
   },
 }
