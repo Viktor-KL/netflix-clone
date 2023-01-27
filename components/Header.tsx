dcf5bad973ae842eca6c20642b6eb5666dc613ea
Viktor-KL <viktorkyrylovych88@gmail.com>
2023-01-27 07:34:02 +0200

    Fix header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,9 @@ function Header() {
     useEffect(() => {
       const handleScroll = () => {
         if (window.scrollY > 0) {
-            setIsScrolled(true)
+          setIsScrolled(true)
         } else {
-            setIsScrolled(false)
+          setIsScrolled(false)
         }
       }
     
@@ -23,7 +23,7 @@ function Header() {
     
 
   return (
-    <header className={`${isScrolled && "bg-[#141414"}`}>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
             <img
             src="https://rb.gy/ulxxee"
